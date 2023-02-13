5f8935b81d20bb64165749b4ba6d603d7d305130
Vlad Trukhan <vlad.trukhan@gmail.com>
2023-02-13 13:30:03 +0300

    langSyntax tasks complete

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int revertNum = 0;
+        for (int i = 0; i < 4; i++) {
+            revertNum += number % 10;
+            number /= 10;
+        }
+        System.out.println(revertNum);
     }
-
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int revertNum = 0;
+        int temp = 100;
+        for (int i = 0; i < 3; i++) {
+            revertNum += number % 10 * temp;
+            number /= 10;
+            temp /= 10;
+        }
+        System.out.println(revertNum);
     }
-
 }
