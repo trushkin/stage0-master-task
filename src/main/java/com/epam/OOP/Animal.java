4d8664842e508cea7ea47456464febd5140219b2
Vlad Trukhan <vlad.trukhan@gmail.com>
2023-02-13 12:58:38 +0300

    OOP tasks complete

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,47 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    // "This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+    public String getDescription() {
+        String wordPaws;
+        wordPaws = this.getNumberOfPaws() > 1 ? "paws" : "paw";
+        String articleBeforeFur;
+        articleBeforeFur = this.isHasFur() ? "a" : "no";
+        return "This animal is mostly " + this.getColor() +". It has "+ this.getNumberOfPaws() + " " + wordPaws + " and " + articleBeforeFur + " fur.";
+    }
 
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,11 @@
 package com.epam.OOP;
 
-public class Bird {
-
+public class Bird extends Animal {
+    public Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    public String getDescription(){
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
